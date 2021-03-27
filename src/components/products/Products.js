9a73a0f1297c2f847e96590078bab2f5e5d172b2
M4V3R1CK <eddymav247@gmail.com>
2021-03-27 17:49:09 +0100

    Commerce API connection established

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -3,25 +3,8 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running Shoes",
-    price: "$5",
-    image:
-      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  },
-  {
-    id: 2,
-    name: "Macbook",
-    description: "Apple Laptop",
-    price: "$50",
-    image:
-      "https://images.macrumors.com/t/eiMSN7qdduLJTuC6UpaOpsF5QfU=/800x0/filters:quality(90)/article-new/2021/01/Flat-2021-MacBook-Pro-Mockup-Feature-1.jpg?lossy",
-  },
-];
-const Products = () => {
+
+const Products = ({ products }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
